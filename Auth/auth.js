94370319cc5db6733c68d9a007babbbed2ce9729
bootsrap <adhiltajudeen@gmail.com>
2023-01-29 00:48:40 +0530

    create login & registration api

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { con } from "../server.js";
 import bcrypt from "bcrypt";
 
@@ -64,8 +65,55 @@ export const login = async (req, res) => {
 >>>>>>> 3dc73c5 (create registration api)
 =======
 import { query } from "../server.js";
+=======
+import { con } from "../server.js";
+import bcrypt from "bcrypt";
+>>>>>>> 21d7caa (create login & registration api)
 
-export const getUsers = (sql, data, res) => {
-  query(sql, data, res);
+export const addUsers = (data, res) => {
+  const { user_name, password, gender } = data;
+  console.log("password", password);
+  const sql = "insert into users set ?";
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(password, salt);
+  con.query(sql, { user_name, password: hash, gender }, (err, result) => {
+    if (err) {
+      res.status(400).send({ error: err.message });
+    } else if (result.affectedRows === 0) {
+      res.status(200).send({ error: null, data: [] });
+    } else {
+      res.status(200).send({ error: null, user_id: result.insertId });
+    }
+  });
+};
+
+export const getUsers = (res) => {
+  const sql = "select * from  users";
+  con.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).send({ error: err.message });
+    } else {
+      res.status(200).send({ error: null, result });
+    }
+  });
+};
+export const login = async (req, res) => {
+  let { user_name, password } = req.body;
+  let sql = `select * from  users WHERE user_name= '${user_name}'`;
+
+  con.query(sql, (err, result) => {
+    const isPassword =
+      result.length === 0
+        ? false
+        : bcrypt.compareSync(password, result[0]?.password);
+
+    if (!isPassword) {
+      return res
+        .status(400)
+        .send({ error: "password or user name  is invalid" });
+    } else {
+      return res.status(200).send({ error: null, data: result });
+    }
+  });
 };
 >>>>>>> ce9e752 (change condition & create new api)
