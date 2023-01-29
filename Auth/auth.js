0a7af1c654be3e037549251a5f8bbad6145241e8
bootsrap <adhiltajudeen@gmail.com>
2023-01-29 10:22:27 +0530

    soled error

--- Auth/auth.js
+++ Auth/auth.js
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 
 export const addUsers = (data, res) => {
   const { user_name, password, gender } = data;
-  console.log("password", password);
   const sql = "insert into users set ?";
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(password, salt);
+
   con.query(sql, { user_name, password: hash, gender }, (err, result) => {
     if (err) {
       res.status(400).send({ error: err.message });
@@ -25,6 +25,26 @@ export const getUsers = (res) => {
       res.status(400).send({ error: err.message });
     } else {
       res.status(200).send({ error: null, result });
+    }
+  });
+};
+
+export const login = async (req, res) => {
+  let { user_name, password } = req.body;
+  let sql = `select * from  users WHERE user_name= '${user_name}'`;
+
+  con.query(sql, (err, result) => {
+    const isPassword =
+      result.length === 0
+        ? false
+        : bcrypt.compareSync(password, result[0]?.password);
+
+    if (!isPassword) {
+      return res
+        .status(400)
+        .send({ error: "password or user name  is invalid" });
+    } else {
+      return res.status(200).send({ error: null, data: result });
     }
   });
 };
